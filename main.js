31d70ddc5ce6e1b409d096814e3254be6e9a41ea
Miguel Palacios <Palacios.1997.miguel@gmail.com>
2022-11-26 10:25:06 +0100

    uploading work

---
+++ main.js
@@ -0,0 +1,8 @@
+let learnLink = document.getElementById('learn-link');
+let learnLinkTwo = document.getElementById('learn-link-two');
+let styleOne = document.getElementById('style-one');
+let styleTwo = document.getElementById('style-two')
+
+// learnLink.addEventListener('mouseover', (e) => {
+//     styleOne = e.target.style.backgroundColor = 'red';
+// })
